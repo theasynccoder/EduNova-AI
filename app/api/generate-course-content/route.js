874a59e1262af4e45f2cdd9744c13c7915e7cbe1
MR火SASUKE火 <154556203+theasynccoder@users.noreply.github.com>
2026-01-01 14:27:46 +0530

    Update AI model from gemini-2.0 to gemini-2.5

--- app/api/generate-course-content/route.js
+++ app/api/generate-course-content/route.js
@@ -37,7 +37,7 @@ export async function POST(req) {
 
       // ---------------- AI CALL ----------------
       const response = await ai.models.generateContent({
-        model: "gemini-2.0-flash",
+        model: "gemini-2.5-flash-lite",
         contents: [
           {
             role: "user",
